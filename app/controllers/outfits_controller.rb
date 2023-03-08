79c00d56bf2d87ac8f9b139bc7c948253c71283c
Adlina Basuki <ad.basuki@gmail.com>
2023-03-08 09:34:19 +0800

    very basic outfits-new and landing

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,5 +1,11 @@
 class OutfitsController < ApplicationController
   def new
+    @user = current_user
+    @occasions = Occasion.all
+    @occasions_name = []
+    @occasions.each do |occasion|
+      @occasions_name << occasion.name
+    end
   end
 
   def create
