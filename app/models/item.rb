a297e9b2557834c91ad046fda833a0a5b4102f9e
jensen drawan <jdrawan@gmail.com>
2023-03-08 15:20:38 +0800

    add now works with multiple occasion

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,10 @@ class Item < ApplicationRecord
   has_many :items_occasions
   has_one_attached :photo
 
+  TYPE_OPTIONS = ["TOP", "BOTTOM", "SHOE"]
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :type, :photo, presence: true
+
   self.inheritance_column = nil
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # destroying all users
+
+Item.destroy_all
 puts "destroying all users"
 User.destroy_all
 puts "all users destroyed"
@@ -17,6 +19,12 @@ Occasion.destroy_all
 puts "all occasions destroyed"
 
 #Creating users
+puts "creating users"
+stefany = User.new(first_name: "Stefany", last_name: "Noguera", email: "stefany@gmail.com", password:"password")
+stefany.save
+puts "created #{User.count} users"
+
+#Creating occasions
 puts "creating occasions"
 Occasion.create(name: "WORK")
 Occasion.create(name: "NIGHT OUT")
@@ -24,8 +32,10 @@ Occasion.create(name: "CASUAL")
 Occasion.create(name: "GYM")
 puts "created #{Occasion.count} occasion"
 
-#Creating users
-puts "creating users"
-stefany = User.new(first_name: "Stefany", last_name: "Noguera", email: "stefany@gmail.com", password:"password")
-stefany.save
-puts "created #{User.count} users"
+# #Creating occasions
+# puts "creating occasions"
+# Occasion.create(name: "WORK", user: stefany)
+# Occasion.create(name: "NIGHT OUT", user: stefany)
+# Occasion.create(name: "CASUAL", user: stefany)
+# Occasion.create(name: "GYM", user: stefany)
+# puts "created #{Occasion.count} occasion"
