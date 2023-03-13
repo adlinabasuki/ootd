0f73924f8b503426bd436eeb7876c8def96e4f96
jensen drawan <jdrawan@gmail.com>
2023-03-13 14:50:55 +0800

    change the layout of the new add item page and some change on affirmation page

--- app/controllers/affirmations_controller.rb
+++ app/controllers/affirmations_controller.rb
@@ -1,5 +1,14 @@
 class AffirmationsController < ApplicationController
   def display
+    phrases = [
+      "You're a fashionista!",
+      "You have a great sense of style!",
+      "Your fashion game is strong!",
+      "You're a trendsetter!",
+      "You're a style guru!",
+    ];
+
+    @affirmation = phrases.sample
     @outfit = Outfit.find(params[:id])
   end
 end
