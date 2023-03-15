7580529e6688e721db55cc59d6c3fa220bc60015
Felicia Tjandinegara <felicia_tk@outlook.com>
2023-03-15 15:08:50 +0800

    weather ui

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,18 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
-  static targets = ["desc", "temp", "city"]
+  static targets = ["temp", "city"]
 
   connect() {
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
       const weatherField = document.querySelector(".weather-temp")
+      const iconField = document.getElementById("weather-icon")
       const descriptionField = document.querySelector(".weather-descr")
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=3383dc5d38c97be1b56c31e88567f2e7`;
       fetch(url)
         .then(response => response.json())
         .then((data) => {
-          this.descTarget.textContent = data.weather[0].main;
+          const iconId = data.weather[0].icon;
+          iconField.src = `https://openweathermap.org/img/w/${iconId}.png`;
           descriptionField.value = data.weather[0].main;
           this.tempTarget.textContent = data.main.temp;
           weatherField.value = data.main.temp;
