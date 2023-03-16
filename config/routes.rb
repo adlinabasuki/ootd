7549827e3f29e33ea9b78f5e990d3ad16c521303
Felicia Tjandinegara <felicia_tk@outlook.com>
2023-03-16 16:50:23 +0800

    ui fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :items
   resources :outfits, only: [:new, :index, :show] do
     member do
