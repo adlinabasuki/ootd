8c6a9b4556fbb05cf7a8865414ccf719330f1c37
jensen drawan <jdrawan@gmail.com>
2023-03-16 15:56:03 +0800

    fix destroy function

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,6 +1,6 @@
 class Outfit < ApplicationRecord
   belongs_to :user
-  has_many :outfits_items
+  has_many :outfits_items, dependent: :destroy
   has_many :items, through: :outfits_items
   has_many :items_occasions, through: :items
   has_many :occasions, through: :items_occasions
